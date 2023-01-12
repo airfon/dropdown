d7fef37c77cccd871c4a926c02f2a9f06a609b5d
airfon <116084172+airfon@users.noreply.github.com>
2023-01-13 01:29:18 +0300

    Add files via upload

--- dropdown.js
+++ dropdown.js
@@ -21,7 +21,7 @@
 				listLi.setAttribute("key",keys[ind]);
 				listUl.appendChild(listLi);
 				listLi.addEventListener("click", event => {
-					trigger = listLi.getAttribute("key");
+					trigger = this.getAttribute("key");
 					var event = new Event("onSelect");
 					this.dispatchEvent(event);
 					console.log(trigger);
