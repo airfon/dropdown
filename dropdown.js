de213fef8dcada5fd3b64a70ff88e66a9723da38
airfon <116084172+airfon@users.noreply.github.com>
2023-01-13 01:05:50 +0300

    Add files via upload

--- dropdown.js
+++ dropdown.js
@@ -7,15 +7,21 @@
 			listBox.setAttribute("class","hoverbox");
 			listBox.innerHTML = "<style>.hoverbox *:last-child{opacity:0;z-index:-1000;transition:opacity .3s .1s,z-index .1s;display: none;}.hoverbox:hover *:last-child{opacity:1;z-index:1;transition:opacity .3s .3s,z-index .1s .6s;display:block}</style><span>Hello World</span>";
 			var listContainer = document.createElement("div");
-			listContainer.setAttribute("width","200px");
-			listContainer.setAttribute("height","300px");
-			listContainer.setAttribute("background-color","white");
-			listContainer.setAttribute("position","absolute");
-			listContainer.setAttribute("top","36px");
-			listContainer.setAttribute("box-shadow","0 0.25rem 0.5rem 0 rgba(59,63,73,0.15)");
+			
+			listContainer.setAttribute("style","width:200px;height:300px;background-color:white;position:absolute;top:36px;box-shadow:0 0.25rem 0.5rem 0 rgba(59,63,73,0.15)");
 			listBox.appendChild(listContainer);
 			this.appendChild(listBox);
-			
+			var keys = ["0","1","2"];
+			var texts = ["text1","text2","text3"];
+			var listUl = document.createElement("ul");
+			for (var ind=0;ind<keys.length;i++) {
+				var listLi = document.createElement("li");
+				listLi.innerHTML = texts[ind];
+				listLi.setAttribute("class","list-li");
+				listLi.setAttribute("key",keys[ind]);
+				listUl.appendChild(listLi);
+			}
+			listContainer.appendChild(listUl);
             this.addEventListener("click", event => {
                 var event = new Event("onClick");
                 this.dispatchEvent(event);
